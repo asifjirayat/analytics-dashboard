f2ac27c0b0c03cdb9637a8ef71afac1fe5870d21
Asif <asifjirayat@gmail.com>
2025-09-15 22:46:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl text-stone-600">
+      React Analytics Dashboard
+    </div>
+  );
+};
+
+export default App;
